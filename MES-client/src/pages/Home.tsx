24b833d16bc1304d49ceec3a4bce45a3a6d59bf1
Sayan Snigdha Pal <sayanpalb@gmail.com>
2025-01-30 00:15:36 +0530

    Home page first two sections done, Routing setup done

---
+++ MES-client/src/pages/Home.tsx
@@ -0,0 +1,70 @@
+import homeImg from "../assets/homeImg.png";
+
+export default function Home() {
+  return (
+    <section className="min-h-screen">
+      <div className="flex justify-center flex-col items-center">
+        {/* 1st section */}
+        <div className="flex flex-col items-center">
+          {/* Image Container */}
+          <div>
+            <div className="w-full h-[400px] mb-8 relative">
+              <img
+                src={homeImg}
+                alt="Mountain expedition"
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute top-[30%] left-[10%]">
+                <p className="text-sm uppercase tracking-wider text-[#FFFFFF] mb-2 text-center">
+                  YOUR BEST VACATION DECISION
+                </p>
+
+                <h1 className="text-2xl md:text-2xl font-bold text-center text-[#FFFFFF]">
+                  Welcome to Mountain <br /> Expedition Supply
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          {/* Content Container */}
+          <div className="max-w-3xl mx-auto text-center px-4">
+            <div className="space-y-6">
+              <h2 className="text-xl font-medium">
+                Meticulously curated outdoor apparel & gear for purchase or rent
+                for the whole family!
+              </h2>
+
+              <p className="text-gray-600">
+                Mountain Expedition Supply is your next basecamp for everything
+                you need to Ski, Camp and Explore! We've got topline skis,
+                snowboards, boots, hiking shoes, sleeping bags, apparel and
+                custom services so you can get out & play!
+              </p>
+
+              <button className="bg-blue-700 text-white px-16 py-2 rounded-full hover:bg-blue-800 transition-colors">
+                SHOP
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Feature section */}
+        <div className="flex flex-col items-start mt-5">
+          <h1 className="text-2xl font-semibold mb-5">Featured</h1>
+          <div className="flex flex-row items-center justify-center gap-5">
+            {/* left side */}
+            <div className="w-[400px] h-[400px] bg-red-500">
+
+            </div>
+            {/* right side */}
+            <div className="flex flex-col bg-blue-500 w-[400px] h-[400px] justify-between items-center px-3">
+              <div className="bg-yellow-400 w-[350px] h-[110px] rounded-md"></div>
+              <div className="bg-yellow-400 w-[350px] h-[110px] rounded-md"></div>
+              <div className="bg-yellow-400 w-[350px] h-[110px] rounded-md"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
