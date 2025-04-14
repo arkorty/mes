55380215d373063bd0ea385908eed0330c8b0051
Prasundas99 <prasundas115@gmail.com>
2025-04-14 22:36:29 +0530

    added ecosystem

---
+++ MES-admin-v2/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: "mes-admin",
+        script: "npm",
+        args: "start",
+        env: {
+          NODE_ENV: "production",
+          PORT: 3998 
+        }
+      }
+    ]
+  }
+  
