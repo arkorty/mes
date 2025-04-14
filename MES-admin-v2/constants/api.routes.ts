dee74fd694cfe65b64153eea6aef930890caac98
Aryesh Saha <aryeshsaha5302@gmail.com>
2025-04-14 12:57:40 +0530

    pushed env to github

--- MES-admin-v2/constants/api.routes.ts
+++ MES-admin-v2/constants/api.routes.ts
@@ -1,25 +1,31 @@
 export const API_ROUTES = {
   orders: ({ page = 1, limit = 8, search = '' }) =>
-    `/order?page=${page}&limit=${limit}&search=${search}`,
-  getOrderById: (id: any) => `/order/${id}`,
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/order?page=${page}&limit=${limit}&search=${search}`,
+  getOrderById: (id: any) =>
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/order/${id}`,
 
   products: ({ page = 1, limit = 8, search = '' }) =>
-    `/product?page=${page}&limit=${limit}&search=${search}`,
-  addUpdateProduct: `/product/upsert`,
-  productDropdown: `/product/dropdown/list`,
-  getProductById: (id: any) => `/product/${id}`,
-  deleteProduct: (id: any) => `/product/${id}`,
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/product?page=${page}&limit=${limit}&search=${search}`,
+  addUpdateProduct: `${process.env.NEXT_PUBLIC_API_BASE_URL}/product/upsert`,
+  productDropdown: `${process.env.NEXT_PUBLIC_API_BASE_URL}/product/dropdown/list`,
+  getProductById: (id: any) =>
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/product/${id}`,
+  deleteProduct: (id: any) =>
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/product/${id}`,
 
-  categories: `/category`,
-  addUpdateCategory: `/category/upsert`,
-  categoryDropdown: `/category/dropdown`,
-  getCategoryById: (id: any) => `/category/${id}`,
-  deleteCategory: (id: any) => `/category/${id}`,
+  categories: `${process.env.NEXT_PUBLIC_API_BASE_URL}/category`,
+  addUpdateCategory: `${process.env.NEXT_PUBLIC_API_BASE_URL}/category/upsert`,
+  categoryDropdown: `${process.env.NEXT_PUBLIC_API_BASE_URL}/category/dropdown`,
+  getCategoryById: (id: any) =>
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/category/${id}`,
+  deleteCategory: (id: any) =>
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/category/${id}`,
 
   users: ({ page = 1, limit = 8, search = '' }) =>
-    `/user/all?page=${page}&limit=${limit}&search=${search}`,
-  getUserById: (id: any) => `/user/${id}`,
-  deleteUser: (id: any) => `/user/${id}`,
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/all?page=${page}&limit=${limit}&search=${search}`,
+  getUserById: (id: any) =>
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/${id}`,
+  deleteUser: (id: any) => `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/${id}`,
 
   adminLogin: `/user/admin/login`
 };
