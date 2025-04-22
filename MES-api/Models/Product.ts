678b5c0c18c96cb6d4c17a58a3782d833a737fd3
Aryesh Saha <aryeshsaha5302@gmail.com>
2025-04-23 02:23:43 +0530

    product addition functionality with variation images and stockmaster collection functionality added

--- MES-api/Models/Product.ts
+++ MES-api/Models/Product.ts
@@ -1,87 +1,107 @@
-import mongoose, { Document, Schema } from 'mongoose';
-import slugify from 'slugify';
+import mongoose, { Document, Schema } from "mongoose";
+import slugify from "slugify";
 
-const dimensionsSchema = new Schema({
-  length: { type: Number, required: true },
-  width: { type: Number, required: true },
-  height: { type: Number, required: true },
-  unit: { type: String, enum: ['cm', 'inch'], default: 'cm' }
-}, { _id: false });
-
-
+const dimensionsSchema = new Schema(
+  {
+    length: { type: Number, required: true },
+    width: { type: Number, required: true },
+    height: { type: Number, required: true },
+    unit: { type: String, enum: ["cm", "inch"], default: "cm" },
+  },
+  { _id: false }
+);
 
 export interface IProduct extends Document {
-    name: string;
-    slug: string;
-    brand: string;
-    categoryId?: string; // ObjectId
-    subCategoryId?: string; // ObjectId
-    subSubCategoryId?: string; // ObjectId
-    description: string;
-    shortDescription: string;
-    specifications: string;
-    isActive: boolean;
-    isFeatured: boolean;
-    tags: string[];
-    productCategoryDepth: number;
-    parentProductCategoryId: string; //immediate parent category
+  name: string;
+  slug: string;
+  brand: string;
+  sku: string;
+  categoryId?: string; // ObjectId
+  subCategoryId?: string; // ObjectId
+  subSubCategoryId?: string; // ObjectId
+  description: string;
+  shortDescription: string;
+  price: string;
+  salePrice: string;
+  specifications: string;
+  isActive: boolean;
+  isFeatured: boolean;
+  tags: string[];
+  productCategoryDepth: number;
+  parentProductCategoryId: string; //immediate parent category
 }
 
-const productSchema = new Schema<IProduct>({
+const productSchema = new Schema<IProduct>(
+  {
     name: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
     slug: {
       type: String,
-      unique: true
+      unique: true,
     },
     brand: {
+      type: String,
+      required: true,
+    },
+    sku: {
       type: String,
       required: true,
     },
     categoryId: {
       type: Schema.Types.ObjectId,
       //required: true,
-      ref: 'Category'
+      ref: "Category",
     },
     subCategoryId: {
       type: Schema.Types.ObjectId,
-      ref: 'Category'
+      ref: "Category",
     },
     subSubCategoryId: {
       type: Schema.Types.ObjectId,
-      ref: 'Category'
+      ref: "Category",
     },
     description: {
       type: String,
-      required: true
+      required: true,
     },
     shortDescription: {
       type: String,
       required: true,
-      maxlength: 200
+      maxlength: 200,
     },
-   specifications:{type:String},
+    price: {
+      type: String,
+      required: true,
+    },
+    salePrice: {
+      type: String,
+      required: true,
+    },
+    specifications: { type: String },
     isActive: {
       type: Boolean,
-      default: true
+      default: true,
     },
-    
+
     isFeatured: {
       type: Boolean,
-      default: false
+      default: false,
     },
-    tags: [{
-      type: String,
-      trim: true
-    }]
-  }, {
+    tags: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+  },
+  {
     timestamps: true,
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-  });
-  
-  
-  export const Product = mongoose.model<IProduct>('Product', productSchema);
+    toObject: { virtuals: true },
+  }
+);
+
+export const Product = mongoose.model<IProduct>("Product", productSchema);
